1d77f3cff8c0b2b363d49659f0e541a70fa7a8f4
Fahrizal Julian Adam <tetsuyakumiko01@gmail.com>
2024-03-08 11:12:39 +0700

    first commit

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,22 @@
 <script setup>
+// Import modul dan komponen yang diperlukan
+import Swal from "sweetalert2";
 import { onMounted, ref, nextTick } from "vue";
-import $ from "jquery"; // Import jQuery
-import "datatables.net"; // Import DataTables
+import $ from "jquery";
+import "datatables.net";
 import sidebar from "@/layouts/sidebar.vue";
+import { useRouter } from "vue-router";
 
+// Deklarasi variabel reaktif
 const users = ref([]);
+const router = useRouter();
 
+// Kode yang dijalankan saat komponen dimounted
 onMounted(async () => {
   try {
-    // Fetch data from API
     const response = await fetch("https://dummyjson.com/users");
     const data = await response.json();
     users.value = data.users;
-
-    // Initialize DataTable after DOM is ready
     await nextTick();
     $("#datatable").DataTable();
   } catch (error) {
@@ -21,25 +24,35 @@ onMounted(async () => {
   }
 });
 
+// Fungsi untuk menghapus pengguna berdasarkan ID
 const deleteUser = async (userId) => {
   try {
-    const response = await fetch(`https://dummyjson.com/users/${userId}`, {
-      method: "DELETE",
+    const confirmResult = await Swal.fire({
+      title: "Apakah anda yakin ingin menghapus data?",
+      showCancelButton: true,
+      confirmButtonText: "Ya",
+      cancelButtonText: "Tidak",
+      icon: "question",
     });
-    if (response.ok) {
-      // Remove the user from the local array
-      users.value = users.value.filter((user) => user.id !== userId);
-    } else {
-      console.error("Failed to delete user.");
+
+    if (confirmResult.isConfirmed) {
+      const response = await fetch(`https://dummyjson.com/users/${userId}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        users.value = users.value.filter((user) => user.id !== userId);
+      } else {
+        console.error("Failed to delete user.");
+      }
     }
   } catch (error) {
     console.error("Error deleting user:", error);
   }
 };
 
-const editUser = async (userId) => {
-  // You can implement your edit functionality here
-  console.log("Edit user with ID:", userId);
+// Fungsi untuk mengarahkan ke halaman edit pengguna
+const editUser = (userId) => {
+  router.push({ path: `/edit/${userId}` });
 };
 </script>
 
@@ -47,25 +60,24 @@ const editUser = async (userId) => {
   <sidebar></sidebar>
   <div class="p-4 sm:ml-64 bg-white pt-10">
     <div class="p-4 bg-white rounded-lg mt-14 h-full space-y-10">
-      <h1 class="text-4xl text-black font-semibold">List Of Costumer</h1>
+      <h1 class="text-4xl text-black font-semibold">List Of Costumers</h1>
       <button
         class="btn bg-red-600 text-white hover:bg-red-900 w-[10rem] rounded-2xl h-10 duration-200"
       >
-        <RouterLink to="#"> Add Data </RouterLink>
+        <router-link to="/add">Add Data</router-link>
       </button>
       <table id="datatable" class="table-auto w-full text-black">
         <thead>
           <tr>
             <th class="!text-start">ID</th>
-            <th class="!text-start">Nama</th>
-            <th class="!text-start">Alamat</th>
-            <th class="!text-start">Kota</th>
-            <th class="!text-start">Tanggal Lahir</th>
+            <th class="!text-start">Name</th>
+            <th class="!text-start">Address</th>
+            <th class="!text-start">City</th>
+            <th class="!text-start">Date of Birth</th>
             <th class="!text-start">Phone</th>
             <th class="!text-start">Email</th>
-            <th class="!text-start">Catatan</th>
-            <th class="!text-start">Setting</th>
-            <!-- Tambah kolom Setting -->
+            <th class="!text-start">Note</th>
+            <th class="!text-start">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -81,20 +93,17 @@ const editUser = async (userId) => {
             <td class="!text-start">{{ user.email }}</td>
             <td class="!text-start">{{ user.note }}</td>
             <td class="!text-start">
-              <!-- Tombol Edit -->
               <button
                 @click="editUser(user.id)"
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
               >
                 Edit
               </button>
-              <!-- Tombol View -->
               <button
                 class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
               >
                 View
               </button>
-              <!-- Tombol Delete -->
               <button
                 @click="deleteUser(user.id)"
                 class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
